7e86f4414ddef99c9a994afc5fbfbfb9fa4cabbb
Мегамозг <@DESKTOP-ISK8TFH>
2026-04-29 23:28:48 +0800

    task 1

--- ConsoleApplication1/Solution.cpp
+++ ConsoleApplication1/Solution.cpp
@@ -1,52 +1,107 @@
 ﻿#include <iostream>
 #include "GL\glew.h"
+#include <cmath>
 #include"GLFW\glfw3.h"
+
+const char* vertexShaderSource = R"(
+    #version 460 core
+    layout (location = 0) in vec2 aPos;
+    void main() {
+        gl_Position = vec4(aPos, 0.0, 1.0);
+    }
+)";
+const char* fragmentShaderSource = R"(
+    #version 460 core
+    out vec4 FragColor;
+    uniform float uTime;
+    void main() {
+        float red = 0.2f + (sin(uTime) * 0.3f);
+        float green = 0.4f + (cos(uTime * 1.3f) * 0.3f);
+        float blue = 1.0f;
+        FragColor = vec4(red, green, blue, 1.0);
+    }
+)";
 int main()
 {
 	if (!glfwInit()) {
 		return 1;
 	}
+
+    float vertices[] = {
+    -0.5f, -0.5f,
+     0.5f, -0.5f,
+     0.0f,  0.5f
+    };
+
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "triangle", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glewInit();
+
     GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    glBindVertexArray(VAO); //
-	GLFWwindow* window = glfwCreateWindow(512, 512, "MainWindow", NULL, NULL);
-	if (!window) {
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	GLenum ret = glewInit();
-	if (GLEW_OK != ret) {
-		return 1;
-	}
-    while (!glfwWindowShouldClose(window)) {
 
-        glClearColor(0.8f, 0.2f, 0.7f, 1.0f);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glClearColor(1.0f, 1.0f, 0.5f, 1.0f);
+
+    int timeLocation = glGetUniformLocation(shaderProgram, "uTime");
+
+    while (!glfwWindowShouldClose(window)) {
+        float timeValue = glfwGetTime();
+
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
+        glUseProgram(shaderProgram);
 
-        glColor3f(0.3, 0.6, 0.1);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0.5, -0.5);
-        glVertex2f(0.5, 0.5);
+        glUniform1f(timeLocation, timeValue);
 
-        glColor3f(0.3, 0.6, 0.1);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0.5, 0.5);
-        glVertex2f(-0.5, 0.5);
-        glEnd();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
-
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
     glfwTerminate();
